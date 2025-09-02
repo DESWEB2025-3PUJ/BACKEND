2b878b70705710b6a78ce50fb65e6dc360b26e10
Aulas Ingenieria <aulasingenieria@ujaverianas.loc>
2025-09-02 12:35:50 -0500

    Subiendo proyecto base con entities, dto, controllers y config

--- src/main/java/com/wikigroup/desarrolloweb/repository/EdgeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.wikigroup.desarrolloweb.repository;
-
-import com.wikigroup.desarrolloweb.model.Edge;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EdgeRepository extends JpaRepository<Edge, Long> {
-}
--- src/main/java/com/wikigroup/desarrolloweb/repository/ProcessRepository.java
+++
@@ -1,7 +0,0 @@
-package com.wikigroup.desarrolloweb.repository;
-
-import com.wikigroup.desarrolloweb.model.Process;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProcessRepository extends JpaRepository<Process, Long> {
-}

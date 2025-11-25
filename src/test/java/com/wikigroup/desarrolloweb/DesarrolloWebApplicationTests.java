2b0ec9bdf9e984b1dd7987ba55d810a69c0965b4
DavidSP12 <dpineros05@gmail.com>
2025-11-25 12:29:38 -0500

    Add TestSecurityConfig import to application test classes

--- src/test/java/com/wikigroup/desarrolloweb/DesarrolloWebApplicationTests.java
+++ src/test/java/com/wikigroup/desarrolloweb/DesarrolloWebApplicationTests.java
@@ -1,11 +1,14 @@
 package com.wikigroup.desarrolloweb;
 
+import com.wikigroup.desarrolloweb.config.TestSecurityConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
+@Import(TestSecurityConfig.class)
 class DesarrolloWebApplicationTests {
 
 	@Test
--- src/test/java/com/wikigroup/proyecto/proyecto/ProyectoApplicationTests.java
+++ src/test/java/com/wikigroup/proyecto/proyecto/ProyectoApplicationTests.java
@@ -1,9 +1,14 @@
 package com.wikigroup.proyecto.proyecto;
 
+import com.wikigroup.desarrolloweb.config.TestSecurityConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest(classes = com.wikigroup.desarrolloweb.DesarrolloWebApplication.class)
+@ActiveProfiles("test")
+@Import(TestSecurityConfig.class)
 class ProyectoApplicationTests {
 
 	@Test
